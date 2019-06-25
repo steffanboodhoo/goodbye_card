58304c9ca4b54509c5dbc6601ef4e56704542ce2
steffanboodhoo <steffan_boodhoo@hotmail.com>
2019-06-24 23:07:20 -0400

    added bootstrap and jquery

--- index.js
+++ index.js
@@ -4,4 +4,5 @@ window.onload = () => {
 
 const steffan_boodhoo_init = () => {
     console.log('I can still access the DOM, don\'t worry')
+    // $('#steffan_boodhoo').append('I can use jquery here')
 }
